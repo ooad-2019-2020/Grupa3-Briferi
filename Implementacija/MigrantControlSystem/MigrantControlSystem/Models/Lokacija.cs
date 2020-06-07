3f5cc02d19d46e6f2dbde70e5220ca266eb1034f
Benjamin Nikšić <bniksic1@etf.unsa.ba>
2020-06-07 16:01:16 +0200

    sitne popravke

--- Implementacija/MigrantControlSystem/MigrantControlSystem/Models/Lokacija.cs
+++ Implementacija/MigrantControlSystem/MigrantControlSystem/Models/Lokacija.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
@@ -9,7 +10,11 @@ namespace MigrantControlSystem.Models
     public class Lokacija
     {
         public int id { get; set; }
+        [Required(ErrorMessage ="Polje x koordinate je obavezno.")]
+        [Display(Name ="X koordinata")]
         public double x { get; set; }
+        [Required(ErrorMessage = "Polje y koordinate je obavezno.")]
+        [Display(Name = "Y koordinata")]
         public double y { get; set; }
     }
 }
--- Implementacija/MigrantControlSystem/MigrantControlSystem/Models/PolicijskaStanica.cs
+++ Implementacija/MigrantControlSystem/MigrantControlSystem/Models/PolicijskaStanica.cs
@@ -10,6 +10,8 @@ namespace MigrantControlSystem.Models
     {
         [Display(Name = "ID")]
         public int id { get; set; }
+        [Required(ErrorMessage = "Polje naziv je obavezno.")]
+        [StringLength(30, ErrorMessage = "Ime mora sadržavati najmanje 5, a najviše 30 slova.", MinimumLength = 5)]
         [Display(Name="Naziv policijske stanice")]
         public String naziv { get; set; }
         public virtual Lokacija lokacija { get; set; }
